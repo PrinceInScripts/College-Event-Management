1106dcbdfdd8638db040324d2cd1a6546ea3ce48
princekumar <princepk7878kumar@gmail.com>
2024-03-27 10:48:42 +0530

    done setup of Localhost PORT and app

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import morgan from "morgan"
+
+const app=express()
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
+app.use(cookieParser())
+app.use(morgan('dev'))
+
+
+export {app}
--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,4 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
 
@@ -5,4 +6,15 @@ dotenv.config({
     path:'.env'
 })
 
+const PORT=process.env.PORT
+
 connectDB()
+        .then(()=>{
+            app.listen(PORT,()=>{
+                console.log(`Server is running at PORT : http://localhost:${PORT}`);
+            })
+        })
+        .catch((error)=>{
+            console.log(error);
+            process.exit(1)
+        })
