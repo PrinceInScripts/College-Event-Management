7b1462a4ec3a414d32ca05cb8a4ae60f58b14d3e
princekumar <princepk7878kumar@gmail.com>
2024-03-27 11:12:20 +0530

    added helper.utils

---
+++ server/src/utils/helper.utils.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import fs from "fs";
+
+
+export const getPaginatedPayload = (dataArray, page, limit) => {
+  const startPosition = +(page - 1) * limit;
+
+  const totalItems = dataArray.length;
+  const totalPages = Math.ceil(totalItems / limit);
+
+  dataArray = structuredClone(dataArray).slice(
+    startPosition,
+    startPosition + limit
+  );
+
+  const payload = {
+    page,
+    limit,
+    totalPages,
+    previousPage: page > 1,
+    nextPage: page < totalPages,
+    totalItems,
+    currentPageItems: dataArray?.length,
+    data: dataArray,
+  };
+  return payload;
+};
+
+export const getMongoosePaginationOptions = ({
+  page = 1,
+  limit = 10,
+  customLabels,
+}) => {
+  return {
+    page: Math.max(page, 1),
+    limit: Math.max(limit, 1),
+    pagination: true,
+    customLabels: {
+      pagingCounter: "serialNumberStartFrom",
+      ...customLabels,
+    },
+  };
+};
+
+export const getRandomNumber = (max) => {
+  return Math.floor(Math.random() * max);
+};
