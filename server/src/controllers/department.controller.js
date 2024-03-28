81a417fc722314337ed57d0b90931bcc29755fb5
princekumar <princepk7878kumar@gmail.com>
2024-03-28 18:59:33 +0530

    done getDepartment methods

--- server/src/controllers/department.controller.js
+++ server/src/controllers/department.controller.js
@@ -5,6 +5,7 @@ import { User } from "../models/user.models.js"
 import { ApiError } from "../utils/ApiError.utils.js"
 import { ApiResponse } from "../utils/ApiResponse.utils.js"
 import { asyncHandler } from "../utils/asyncHandler.utils.js"
+import { getMongoosePaginationOptions } from "../utils/helper.utils.js"
 
 const createDepartment=asyncHandler(async (req,res)=>{
 
@@ -36,7 +37,59 @@ const createDepartment=asyncHandler(async (req,res)=>{
              )
 })
 
+const getAllDepartment=asyncHandler(async (req,res)=>{
+    const {page=1,limit=10}=req.query;
+
+    const departmentAggregate=Department.aggregate([{$match:{}}])
+
+    const departments=await Department.aggregatePaginate(
+        departmentAggregate,
+        getMongoosePaginationOptions({
+            page,
+            limit,
+            customLabels:{
+                totalDocs:"Departments",
+                docs:"department"
+            }
+        })
+    )
+
+
+    return res
+             .status(201)
+             .json(
+                new ApiResponse(
+                    201,
+                    departments,
+                    "Departments fetched successfully"
+                )
+             )
+})
+
+const getDepartmentById=asyncHandler(async(req,res)=>{
+    const {departmentId}=req.params
+
+    const department=await Department.findById(departmentId)
+
+    if(!department){
+        throw new ApiError(404,"Department not found")
+    }
+
+    return res
+             .status(201)
+             .json(
+                new ApiResponse(
+                    201,
+                    department,
+                    "Department fetched successfully"
+                )
+             )
+})
+
 
 export {
-    createDepartment
+    createDepartment,
+    getAllDepartment,
+    getDepartmentById
+    
 }
