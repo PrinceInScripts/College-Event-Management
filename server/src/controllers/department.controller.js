999fad68c0808bf5000a5a62f45cee0cd13103bb
princekumar <princepk7878kumar@gmail.com>
2024-03-30 18:00:42 +0530

    done manageJoinRequest controller

--- server/src/controllers/department.controller.js
+++ server/src/controllers/department.controller.js
@@ -243,6 +243,63 @@ const requestToJoinDepartment=asyncHandler(async (req,res)=>{
 
 })
 
+const manageJoinRequest=asyncHandler(async (req,res)=>{
+    const {departmentId,userId,action}=req.body;
+
+    const user=await User.findById(req.user._id);
+
+    const department=await Department.findById(departmentId)
+
+    if(!department){
+        throw new ApiError(404,"Department not found")
+    }
+
+    if(user.role !== "ADMIN" || user._id !== department.head.toString){
+        throw new ApiError(403,"Unauthorized access")
+    }
+
+    const userToJoin=await User.findById(userId)
+
+    if(!userToJoin){
+        throw new ApiError(404,"User not found")
+    }
+
+    if(!department.pendingRequest.includes(userId)){
+        throw new ApiError(400,"User has no pending request to join the department");
+    }
+
+    if(action === "APPROVE"){
+        department.students.push(userId)
+        department.pendingRequest=department.pendingRequest.filter(id=>id.toString() !== userId)
+        await department.save({validateBeforeSave:false})
+
+        return res
+                 .status(201)
+                 .json(
+                    new ApiResponse(
+                        201,
+                        {},
+                        "User approved to join department"
+                    )
+                 )
+    } else if(action ==="REJECT"){
+      department.pendingRequests = department.pendingRequests.filter(id => id.toString() !== userId);
+      await department.save({validateBeforeSave:false})
+
+      return res
+                 .status(201)
+                 .json(
+                    new ApiResponse(
+                        201,
+                        {},
+                        "User request to join department rejected"
+                    )
+                 )
+    } else {
+           throw new ApiError(400,"Invalid Action")
+    }
+})
+
 export {
     createDepartment,
     getAllDepartment,
@@ -251,5 +308,6 @@ export {
     deleteDepartmentById,
     addStaffToDepartment,
     assignHeadToDepartment,
-    requestToJoinDepartment
+    requestToJoinDepartment,
+    manageJoinRequest
 }
